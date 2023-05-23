a4595fcf6a526894df8074f031547975c74dfd05
RobWPink <RobWPink>
2023-05-23 10:08:44 -0400

    on-off in upload

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+python3 ~/OFF.py
 echo compiling...
 
 arduino-cli compile --fqbn arduino:avr:mega ~/$1
@@ -11,10 +11,11 @@ if [ -z "$2" ]
 then
 arduino-cli upload -v -p /dev/ttyACM0 --fqbn arduino:avr:mega ~/$1
 else
-arduino-cli upload -v -p $2 --fqbn arduino:avr:mega ~/$1
+arduino-cli upload -p $2 --fqbn arduino:avr:mega ~/$1
 fi
 
 echo upload successful!
+python3 ~/ON.py
 echo opening serial monitor...
 if [ -z "$2" ]
 then
@@ -22,4 +23,3 @@ arduino-cli monitor -p /dev/ttyACM0
 else
 arduino-cli monitor -p $2
 fi
-
