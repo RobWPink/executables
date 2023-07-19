58a95a5eef372929d0f53ca81c5edc34b2f3dcdf
Ref1 <robert.pink@oneh2.com>
2023-07-19 10:19:21 -0400

    startupChange

--- startup.sh
+++ startup.sh
@@ -6,8 +6,9 @@ do
 	then
 		python3 /home/defaultUser/ON.py
 		sleep 5
-		screen -dmS server python3 /home/defaultUser/raspiThingsboard/raspiThingsboard.py testreformer
-		screen -dmS autossh sshpass -P "Enter passphrase for key '/home/defaultUser/Thingsboard_key.pem':" -p "oneh2" -v autossh -R 5000:localhost:22 -N 34.236.51.120 -l ubuntu -o ServerAliveInterval=30 -i /home/defaultUser/Thingsboard_key.pem &
+		screen -dmS server python3 /home/defaultUser/raspiThingsboard/raspiThingsboard.py -u reformer1_pad -d
+#		autossh -R 5001:localhost:22 -N 34.236.51.120 -l ubuntu &
+#		screen -dmS autossh sshpass -P "Enter passphrase for key '/home/defaultUser/Thingsboard_key.pem':" -p "oneh2" -v autossh -R 5002:localhost:22 -N 34.236.51.120 -l ubuntu -o ServerAliveInterval=30 -i /home/defaultUser/Thingsboard_key.pem
 		break
 	fi
 done
