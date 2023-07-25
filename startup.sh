37a05d91ffd4b70b7a1953c077819950033c158d
Ref1 <robert.pink@oneh2.com>
2023-07-25 10:46:48 -0400

    reverse tunnel

--- startup.sh
+++ startup.sh
@@ -7,8 +7,7 @@ do
 		python3 ON.py
 		sleep 5
 		screen -dmS server python3 /home/defaultUser/raspiThingsboard/raspiThingsboard.py -u reformer1_pad -d
-#		autossh -R 5001:localhost:22 -N 34.236.51.120 -l ubuntu &
-#		screen -dmS autossh sshpass -P "Enter passphrase for key '/home/defaultUser/Thingsboard_key.pem':" -p "oneh2" -v autossh -R 5002:localhost:22 -N 34.236.51.120 -l ubuntu -o ServerAliveInterval=30 -i /home/defaultUser/Thingsboard_key.pem
+		ssh -fN -R 5001:localhost:22 ubuntu@34.236.51.120 -F sshd_config
 		break
 	fi
 done
